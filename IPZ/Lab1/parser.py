dc5e5c7f5d5555922c6ee1b1c85563f99ffdcd98
adidus <didusavd@gmail.com>
2019-04-06 15:57:32 +0300

    update to Syntax analyser

---
+++ IPZ/Lab1/parser.py
@@ -0,0 +1,257 @@
+from vocabulary import *
+from tree import *
+from token import *
+
+error_table = {"Wrong delimiter": -1, "Wrong key_word": -2, "No such identifier": -3, "Wrong integer": -4}
+
+lex_list = lexemes
+tree = Tree()
+
+
+def scan(dictionary, value):
+    for key, v in dictionary.items():
+        if v == value:
+            return key
+
+
+def err(err_number):  # TODO line and col
+    tree.add(err_number)
+    tree.current_element = tree.current_element.parent_element
+    tree.print_tree()
+    print(scan(error_table, err_number))
+    quit()
+
+
+def functionIdentifier(i):
+    tree.add('function-identifier')
+    identifier(lex_list[i])
+    # tree.current_element = tree.current_element.parent_element
+    return i + 1
+
+
+def unsignedInteger(i):
+    tree.add('unsigned-integer')
+    lexem = lex_list[i]
+    if lexem.code < 1000 and lexem.code >= 500:
+        tree.add(scan(consts, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-3)
+    tree.current_element = tree.current_element.parent_element
+
+
+def constant(i):
+    tree.add('constant')
+    unsignedInteger(i)
+    tree.current_element = tree.current_element.parent_element
+    # tree.current_element = tree.current_element.parent_element
+    return i + 1
+
+
+def functionCharasteristic(i):
+    tree.add('function-charasteristic')
+    lexem = lex_list[i]
+
+    if lexem.code == 92:
+        tree.add(scan(delims, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+    print(lexem.code)
+    unsignedInteger(i + 1)
+
+    i += 2
+    lexem = lex_list[i]
+
+    if lexem.code == 44:
+        tree.add(scan(delims, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+
+    unsignedInteger(i + 1)
+    tree.current_element = tree.current_element.parent_element
+    return i + 2
+
+
+def function(i):
+    tree.add('function')
+
+    i = functionIdentifier(i)
+
+    lexem = lex_list[i]
+    if lexem.code == 61:
+        tree.add(scan(delims, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+
+    i = constant(i + 1)
+    i = functionCharasteristic(i)
+
+    lexem = lex_list[i]
+
+    if lexem.code == 59:
+        tree.add(scan(delims, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+
+    i += 1
+    tree.current_element = tree.current_element.parent_element
+
+    return i
+
+
+def functionList(i):
+    tree.add('function-list')
+    lexem = lex_list[i]
+    if lexem.code == 403:
+        tree.add('empty')
+        tree.current_element = tree.current_element.parent_element
+    else:
+        if lex_list[i].code != 402:
+            i = function(i)
+            i = functionList(i)
+            tree.current_element = tree.current_element.parent_element
+
+    return i
+
+
+def mathFunctionDeclarations(i):
+    tree.add('math-function-declarations')
+    lexem = lex_list[i]
+    if lexem.code == 402:
+        tree.add(scan(keywords, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+        i = functionList(i + 1)
+        tree.current_element = tree.current_element.parent_element
+    else:
+        if lexem.code == 403:
+            tree.add('empty')
+            tree.current_element = tree.current_element.parent_element
+        else:
+            err(-2)
+        tree.current_element = tree.current_element.parent_element
+    tree.current_element = tree.current_element.parent_element
+
+    return i
+
+
+def declarations(i):
+    tree.add('declarations')
+
+    i = mathFunctionDeclarations(i)
+    tree.current_element = tree.current_element.parent_element
+    return i
+
+
+def statement_list(i):
+    tree.add('statement-list')
+
+    lexem = lex_list[i]
+    if lexem.code == 404:
+        tree.add('empty')
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-2)
+    tree.current_element = tree.current_element.parent_element
+    # i+=1
+    return i
+
+
+def block(i):
+    tree.add('block')
+
+    i = declarations(i)
+
+    lexem = lex_list[i]
+
+    if lexem.code == 403:
+        tree.add(scan(keywords, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-2)
+    i += 1
+    i = statement_list(i)
+    lexem = lex_list[i]
+    if lexem.code == 404:
+        tree.add(scan(keywords, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-2)
+    tree.current_element = tree.current_element.parent_element
+    return i;
+
+
+def identifier(lexem):
+    tree.add('identifier')
+
+    if lexem.code >= 1000:
+        tree.add(scan(identifires, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-3)
+    tree.current_element = tree.current_element.parent_element
+    tree.current_element = tree.current_element.parent_element
+
+
+def procedureIdentifier(lexem):
+    tree.add('procedure-identifier')
+    identifier(lexem)
+
+
+def program():
+    tree.add('PROCEDURE')
+    i = 0
+    lexem = lex_list[i]
+    if lexem.code == 401:
+        tree.add(scan(keywords, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+
+    i += 1
+    lexem = lex_list[i]
+    procedureIdentifier(lexem);
+
+    i += 1
+    lexem = lex_list[i]
+
+    if lexem.code == 59:
+        tree.add(scan(delims, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+
+    i += 1
+    i = block(i)
+    i += 1
+    lexem = lex_list[i]
+
+    if lexem.code == 46:
+        tree.add(scan(delims, lexem.code))
+        tree.current_element = tree.current_element.parent_element
+    else:
+        err(-1)
+
+
+def signal_program():
+    if lex_list:
+        program()
+
+        for lex in lex_list:
+            print(lex.value)
+        print('-----------------------------------------')
+        print('-----------------------------------------')
+        print('-----------------------------------------')
+        tree.print_tree()
+        print()
+        print(error_table)
+        print()
+        tree.listing()
+    return tree
+
+# if __name__ == '__main__':
+#     print(lex_list)
+#     signal_program_proc()
---
+++ IPZ/Lab1/tree.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, val, per_elem):
+        self.val = val
+        self.parent_element = per_elem
+        self.leaves = []
+
+
+class Tree:
+    def __init__(self):
+        self.root = Node("signal-program", None)
+        self.current_element = self.root
+
+    def add(self, val):
+        tmp_cur_elem = Node(val, self.current_element)
+        self.current_element.leaves.append(tmp_cur_elem)
+        self.current_element = tmp_cur_elem
+
+    def print_tree(self):
+
+        if self.root is not None:
+            self._print_tree(self.root, 0)
+
+    def _print_tree(self, root, depth):
+        for i in range(0, depth):
+            print('', end='     ')
+        if root.leaves:
+            print('<',root.val,'>')
+        else:
+            print(root.val)
+        for leave in root.leaves:
+            self._print_tree(leave, depth+1)
+
+    def listing(self):
+        if self.root is not None:
+            self._listing(self.root)
+
+    def _listing(self, root):
+        if not root.leaves:
+            if root.val == "empty":
+                return
+            if root.val == ';':
+                print(root.val)
+            else:
+                print(root.val, end=" ")
+        for leave in root.leaves:
+            self._listing(leave)
+
+#if __name__ == '__main__':
+#    tree = Tree()
+#    tree.add('program')
+#    tree.add('PROGRAM')
+    #tree.current_element = tree.current_element.parent_element
+#   tree.add('identifier')
+#    tree.add('MONOGRAM')
+    #tree.current_element = tree.current_element.parent_element
+    #tree.current_element = tree.current_element.parent_element
+#    tree.add(';')
+#    tree.print_tree()
