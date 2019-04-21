93381be16d0709be68032e567eb4da73a49de142
adidus <didusavd@gmail.com>
2019-04-21 19:07:18 +0300

    writing scripts for rendering data

--- DBases/Lab1/scripts/createTables.sql
+++ DBases/Lab1/scripts/createTables.sql
@@ -1,32 +1,42 @@
-CREATE TABLE Bicycles (
-  bicycle_id          SERIAL PRIMARY KEY,
-  brand               VARCHAR(80) NOT NULL,
-  model               VARCHAR(80) NOT NULL,
-  road_legal          boolean     NOT NULL,
-  weight              INTEGER     NOT NULL,
-  type_id             INTEGER     NOT NULL,
-  FOREIGN KEY (type_id)
-  REFERENCES Typess (type_id)
+CREATE TABLE Roads (
+    road_id     SERIAL PRIMARY KEY,
+    name        VARCHAR(80) NOT NULL
 );
-
 
 CREATE TABLE Typess (
-  type_id             SERIAL PRIMARY KEY,
-  type_road           VARCHAR(80) NOT NULL,
-  weight_class        VARCHAR(80) NOT NULL
+    type_id             SERIAL PRIMARY KEY,
+    weight              INTEGER     NOT NULL,
+    road_id             INTEGER     NOT NULL,
+    FOREIGN KEY (road_id)
+    REFERENCES Roads (road_id)
 );
+
+
+INSERT INTO Roads (name) VALUES ('Tarmac'), ('Gravel'), ('Snow');
+
+CREATE TABLE Bicycles (
+    bicycle_id          SERIAL PRIMARY KEY,
+    brand               VARCHAR(80) NOT NULL,
+    model               VARCHAR(80) NOT NULL,
+    road_legal          boolean     NOT NULL,
+    type_id             INTEGER,
+    FOREIGN KEY (type_id)
+    REFERENCES Typess (type_id)
+);
+
+
 
 
 CREATE TABLE Owners (
-  owner_id            SERIAL PRIMARY KEY,
-  name                VARCHAR(80) NOT NULL UNIQUE,
-  age                 INTEGER     NOT NULL,
-  pro                 BOOLEAN     NOT NULL,
-  bicycle_id          INTEGER,
-  type_id             INTEGER     NOT NULL,
-  FOREIGN KEY (bicycle_id)
-  REFERENCES Bicycles (bicycle_id)
-  ON DELETE SET NULL,
-  FOREIGN KEY (type_id)
-  REFERENCES Typess (type_id)
+    owner_id            SERIAL PRIMARY KEY,
+    name                VARCHAR(80) NOT NULL UNIQUE,
+    age                 INTEGER     NOT NULL,
+    pro                 BOOLEAN     NOT NULL,
+    bicycle_id          INTEGER,
+    type_id             INTEGER,
+    FOREIGN KEY (bicycle_id)
+    REFERENCES Bicycles (bicycle_id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (type_id)
+    REFERENCES Typess (type_id)
 );
--- DBases/Lab1/scripts/generateData.sql
+++ DBases/Lab1/scripts/generateData.sql
@@ -1,19 +1,32 @@
-INSERT INTO clubs(name, creation_date, number_of_trophies)
-(SELECT md5(random()::text),
-        (CURRENT_DATE -(random() * (CURRENT_DATE -'01.01.1850'))::int),
-        trunc(random() * 29 + 1) FROM generate_series(1, 300));
+INSERT INTO Typess(weight, road_id)
+    (SELECT (trunc(random() + 1)),
+            (SELECT road_id FROM Roads ORDER BY random() LIMIT 1) FROM generate_series(1, 3));
 
-
-INSERT INTO players(first_name, last_name, date_of_birth, is_injured, height, position_id, club_id)
-(SELECT md5(random()::text),
-        md5(random()::text),
-        ('01.01.2000'::date -(random() * ('01.01.2000'::date -'01.01.1975'::date))::int),
-        random() > 0.5,
-        trunc(random() * 39 + 160),
-        trunc(random() * 3 + 1),
-        (SELECT club_id FROM clubs ORDER BY random() LIMIT 1) FROM generate_series(1, 4000));
-
-
-INSERT INTO tournaments(name, description)
+INSERT INTO Bicycles (brand, model, road_legal, type_id)
     (SELECT md5(random()::text),
-        md5(random()::text) FROM generate_series(1, 1000));
+            md5(random()::text),
+            cast(cast(random() as integer) as boolean),
+            (SELECT type_id FROM Typess ORDER BY random() LIMIT 1) FROM generate_series(1, 1));
+
+INSERT INTO Owners (name, age, pro, bicycle_id, type_id)
+    (SELECT md5(random()::text),
+            (trunc(random() + 10)),
+            (cast(cast(random() as integer) as boolean)),
+            (SELECT bicycle_id FROM Bicycles ORDER BY random() LIMIT 1),
+            (SELECT type_id FROM Typess ORDER BY random() LIMIT 1) FROM generate_series(1, 4));
+
+
+--
+-- INSERT INTO players(first_name, last_name, date_of_birth, is_injured, height, position_id, club_id)
+-- (SELECT md5(random()::text),
+--         md5(random()::text),
+--         ('01.01.2000'::date -(random() * ('01.01.2000'::date -'01.01.1975'::date))::int),
+--         random() > 0.5,
+--         trunc(random() * 39 + 160),
+--         trunc(random() * 3 + 1),
+--         (SELECT club_id FROM clubs ORDER BY random() LIMIT 1) FROM generate_series(1, 4000));
+--
+--
+-- INSERT INTO tournaments(name, description)
+--     (SELECT md5(random()::text),
+--         md5(random()::text) FROM generate_series(1, 1000));
